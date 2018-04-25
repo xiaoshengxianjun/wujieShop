57201ddf60c0c546b10aedf1ee4584d897d203cf
MSI\xsxjl <xsxjlzy@163.com>
2018-04-25 10:28:24 +0800

    添加购物车

--- pages/seckill/seckill.js
+++ pages/seckill/seckill.js
@@ -12,19 +12,52 @@ Page({
     }, {
       time: "10:00",
       status: "抢购中",
-      active: true      
+      active: true
     }, {
       time: "10:00",
       status: "即将开始",
-      active: false      
+      active: false
     }, {
       time: "10:00",
       status: "明天即将开始",
-      active: false 
+      active: false
     }, {
       time: "10:00",
       status: "明天即将开始",
-      active: false 
+      active: false
+    }],
+    goodsList: [{
+      id: 1,
+      img: "https://gomoretech2.oss-cn-shanghai.aliyuncs.com/demo-goods5.jpg",
+      name: "信诚(赣南甜橙)精选 净果哈哈哈哈",
+      desc: "酸甜适度 化渣汁多 精品水果",
+      nowPrice: "59.90",
+      oldPrice: "90.90",
+      remain: 0
+    }, {
+      id: 2,
+      img: "https://gomoretech2.oss-cn-shanghai.aliyuncs.com/demo-goods5.jpg",
+      name: "信诚(赣南甜橙)精选 净果哈哈哈哈",
+      desc: "酸甜适度 化渣汁多 精品水果",
+      nowPrice: "59.90",
+      oldPrice: "90.90",
+      remain: 3
+    }, {
+      id: 3,
+      img: "https://gomoretech2.oss-cn-shanghai.aliyuncs.com/demo-goods5.jpg",
+      name: "信诚(赣南甜橙)精选 净果哈哈哈哈",
+      desc: "酸甜适度 化渣汁多 精品水果",
+      nowPrice: "59.90",
+      oldPrice: "90.90",
+      remain: 7
+    }, {
+      id: 4,
+      img: "https://gomoretech2.oss-cn-shanghai.aliyuncs.com/demo-goods5.jpg",
+      name: "信诚(赣南甜橙)精选 净果哈哈哈哈",
+      desc: "酸甜适度 化渣汁多 精品水果",
+      nowPrice: "59.90",
+      oldPrice: "90.90",
+      remain: 9
     }]
   },
 
